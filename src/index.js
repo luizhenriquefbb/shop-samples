c04dc4a7f891d6ea91fe7bacbc728685f6e09183
Luiz Henrique Freire Barros <luizhenriquefbb@gmail.com>
2018-08-23 11:55:45 -0300

    hover

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-var template = $('#handlebar').html();
+template = $('#handlebar').html();
 // var template = String(document.getElementsByTagName("HTML")[0])
 // var template = document.getElementsByTagName("#handlebar")[0]
 
@@ -7,16 +7,16 @@ var template = $('#handlebar').html();
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 binded = {
     "products": [
-        { 'id': 0, 'name': 'product0', 'price': '0,00', 'category': 'category 1', 'image_src': 'images/productImage1.png' },
-        { 'id': 1, 'name': 'product1', 'price': '1,00', 'category': 'category 1', 'image_src': 'images/productImage1.png' },
-        { 'id': 2, 'name': 'product2', 'price': '2,00', 'category': 'category 2', 'image_src': 'images/productImage1.png' },
-        { 'id': 3, 'name': 'product3', 'price': '3,00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
-        { 'id': 4, 'name': 'product4', 'price': '4,00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
-        { 'id': 5, 'name': 'product5', 'price': '5,00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
-        { 'id': 6, 'name': 'product6', 'price': '6,00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
-        { 'id': 7, 'name': 'product7', 'price': '7,00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
-        { 'id': 8, 'name': 'product8', 'price': '8,00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
-        { 'id': 9, 'name': 'product9', 'price': '9,00', 'category': 'category 3', 'image_src': 'images/productImage1.png' }
+        { 'id': 0, 'name': 'product0', 'price': '0.50', 'category': 'category 1', 'image_src': 'images/productImage1.png' },
+        { 'id': 1, 'name': 'product1', 'price': '1.00', 'category': 'category 1', 'image_src': 'images/productImage1.png' },
+        { 'id': 2, 'name': 'product2', 'price': '2.00', 'category': 'category 2', 'image_src': 'images/productImage1.png' },
+        { 'id': 3, 'name': 'product3', 'price': '3.00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
+        { 'id': 4, 'name': 'product4', 'price': '4.00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
+        { 'id': 5, 'name': 'product5', 'price': '5.00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
+        { 'id': 6, 'name': 'product6', 'price': '6.00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
+        { 'id': 7, 'name': 'product7', 'price': '7.00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
+        { 'id': 8, 'name': 'product8', 'price': '8.00', 'category': 'category 3', 'image_src': 'images/productImage1.png' },
+        { 'id': 9, 'name': 'product9', 'price': '9.00', 'category': 'category 3', 'image_src': 'images/productImage1.png' }
     ],
 
     "shop_list": [],
@@ -34,38 +34,51 @@ binded = {
         return list_of_categories;
     },
 
-   
-};
+
+}
 
 
 function add_to_cart(product_index) {
     var product = binded.products[product_index];
-    console.log("product", product);
-    
+
     // check if already exists
     for (let index = 0; index < binded.shop_list.length; index++) {
         let shop_product = binded.shop_list[index];
         if (product.id == shop_product.product.id) {
             shop_product.qty += 1;
-
+            refresh_price(get_price_total());
             return; // exit function
         }
     }
 
     binded.shop_list.push({ "product": product, "qty": 1 });
+    refresh_price(get_price_total());
+}
+
+function refresh_price(total) {
+    var x = document.getElementsByClassName("shop_list_price");
+    for (var i = 0; i < x.length; i++) {
+        x[i].innerHTML = total;
+    }
+
+    var table = document.getElementById("SL_hover_table");
+    table.innerHTML = ''
+    for (const sp of binded.shop_list) {
+        var row = table.insertRow(-1);
+        row.insertCell(0).innerHTML = sp.product.name;
+        row.insertCell(1).innerHTML = sp.qty;
+        row.insertCell(2).innerHTML = sp.product.price;
+    }
 }
 
 /** Sums total to pay  */
-function get_price_total () {
+function get_price_total() {
     var total = 0;
     for (var list_item of binded.shop_list) {
         total += parseFloat(list_item.product.price) * parseFloat(list_item.qty);
     }
 
-    console.log("total", total);
     return total;
-    
-
 }
 
 
