ba7b2709968dbd19ed730ef27cc611b8cd921163
janyelson <janyelsonvictor@gmail.com>
2018-09-24 17:44:57 -0300

    First commit

---
+++ shop.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var cookieParser = require('cookie-parser');
+
+var shop = require('./routes/index');
+
+app.engine('handlebars',exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/admin', admin);
+app.use('/',shop);
+
+app.liste(3000);
+
+module.exports = app;
