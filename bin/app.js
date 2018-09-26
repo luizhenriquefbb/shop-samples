31b2014b99f0fc9d78e8846eeeeb96c02d6454ff
janyelson <janyelsonvictor@gmail.com>
2018-09-25 21:00:17 -0300

    Mudança no layout

--- bin/app.js
+++ bin/app.js
@@ -1,10 +1,8 @@
 var app = require('../shop');
 var models = require("../models");
 
-app.set('port', 3000);
-
 models.sequelize.sync().then(function () {
-  var server = app.listen(app.get('port'), function() {
+  var server = app.listen( 3000, function() {
     console.log('Express server listening on port ' + server.address().port);
   });
 });
