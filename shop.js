cee6ade0f84cdcbe33946678c0794e78350e9091
Rhenan <rhenancarvalhobra@gmail.com>
2018-09-28 15:06:48 -0300

    Adicionar express-session e redis

--- shop.js
+++ shop.js
@@ -3,7 +3,15 @@ var app = express();
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-
+var cookie = require('cookie-parser');
+var session  = require('express-session'),
+    RedisStore = require('connect-redis')(session);
+    
+app.use(session({store: new RedisStore({'host':config.redisHost, 'port':6379}),
+    secret: 'qA5JrwUCTZuqTAEPEZMhaMWq',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 } }));
 
 
 var admin = require('./routes/admin');
